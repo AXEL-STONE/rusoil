dbf40f3c720ba03def034b11de5f502cc1d74b92
Evgenii Dmitriev <johndmitriev@adn.agency>
2022-09-29 13:08:42 +0700

    Add page

---
+++ test_tasks/rusoil/index.php
@@ -0,0 +1,9 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Тествое задание для ООО РУСОЙЛ");
+?>
+
+
+ 
+
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
